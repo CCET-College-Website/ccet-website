ccc2e970a40f5caa2a82fe905d4e2a7ad434a8a5
Abhay-1704 <abhaysps2004@gmail.com>
2025-06-22 02:14:25 +0530

    Added Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Header from './components/header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Loader from './components/Loader/Loader.jsx';
+import NoPage from './pages/Nopage/NoPage.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './index.css';
-import NoPage from './pages/Nopage/NoPage.jsx';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NoPage/>} />
-        {/* Add more routes here */}
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Showing loader for ~4.5s
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 4500); // animation timing
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Loader />;
+    }
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NoPage />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
